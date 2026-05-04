886b42f6adc6e92f09359f35f74ac37a54415c14
Shonnairn <shonnairn.business@gmail.com>
2026-05-04 16:19:06 -0500

    Understanding data types but ran out of time. rip

---
+++ whatKindOfDataIsYourType/main.py
@@ -0,0 +1,17 @@
+# Variable = It is basically a container for storing data values. 
+#            It is a fundamental concept in programming that allows you to store and 
+#            manipulate data in your code.
+
+# This below is what you call the "string" data type.
+# We call them characters. However, numbers can be store in here as well.
+first_name = "John"
+food = "pizza"
+email = "email@example.com"
+
+print(f"The name is {first_name}.")
+print(f"Personally, I do not mind {food}!")
+print(f"However, if you believe that's dumb? Feel free to reach out to me at {email}!")
+
+# As for intergers, they're what we identify numbers in code.
+age = 22
+print(f"You're at least {age} years old!")
