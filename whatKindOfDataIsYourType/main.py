04263f83920f4d393c92238ccb1140bcab06506c
Shonnairn <shonnairn.business@gmail.com>
2026-05-05 03:57:53 -0500

    Okay messed with the data types same like JS

--- whatKindOfDataIsYourType/main.py
+++ whatKindOfDataIsYourType/main.py
@@ -14,4 +14,42 @@ print(f"However, if you believe that's dumb? Feel free to reach out to me at {em
 
 # As for intergers, they're what we identify numbers in code.
 age = 22
+quantity = 2
+num_of_students = 0
+
 print(f"You're at least {age} years old!")
+print(f"Do you want to buy a total of {quantity} laptops?")
+print(f"Imagine having {num_of_students} students who failed the quiz? Couldn't be me!")
+
+# Floats are intergers however they are the equivalent to decimals
+price_of_stove = 5034.99
+gpa = 3.75
+marathon_distance = 26.2
+
+print(f"Our stove is actually on sale for ${price_of_stove} dollars! What a steal!")
+print(f"I think my gpa was around {gpa}")
+print(f"Did you know I actually ran a total of {marathon_distance} miles in the LA marathon in 2020?")
+
+# Booleans is a data type of either 1 or 2. True or false
+
+is_student = False
+is_kitchenAid_on_promos = True
+is_online = False
+
+if is_student:
+    print("Shonnairn is currently a student.")
+else:    
+    print("Shonnairn is not currently a student.")
+# print(f"Is shonnairn a student currently? {is_student}")
+
+if is_kitchenAid_on_promos:
+    print("KitchenAid is currently on promos!")
+else:
+    print("KitchenAid does not have any ongoing promotions currently.")
+
+if is_online:
+    print(f"Shonnairn currently just went live!")
+else:
+    print(f"Shonnairn is currently offline.")
+
+
