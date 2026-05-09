51e4ae748b1f30bbd8b85b15c080b84f2d07bae0
Shonnairn <shonnairn.business@gmail.com>
2026-05-09 13:03:48 -0500

    elif is new

--- ifStatements/main.py
+++ ifStatements/main.py
@@ -3,9 +3,12 @@
 
 age = int(input("Enter your age:"))
 
+if age >= 100:
+    print("You're too old to sign up.")
 if age >= 18:
     print("You are now signed up for a bank account!")
+elif age <= 13:
+    print("You're too young to sign up for an account.")
 else:
     print("Unfortunately, you're not old enough to sign up for an account. Please come back when you're 18.")
 
-    
