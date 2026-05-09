5c65695fad4ab374fed62c6e294f4823c8e12fd6
Shonnairn <shonnairn.business@gmail.com>
2026-05-09 13:01:40 -0500

    Yeah if statements is the same as js

---
+++ ifStatements/main.py
@@ -0,0 +1,11 @@
+# if = do some code only IF some conditions is True
+# Else do something else
+
+age = int(input("Enter your age:"))
+
+if age >= 18:
+    print("You are now signed up for a bank account!")
+else:
+    print("Unfortunately, you're not old enough to sign up for an account. Please come back when you're 18.")
+
+    
