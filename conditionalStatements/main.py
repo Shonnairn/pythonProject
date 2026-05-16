11bed41e7ba7dd2b23cf147f190972647138cd41
Shonnairn <shonnairn.business@gmail.com>
2026-05-16 03:03:22 -0500

    ternary operators. Must easier for conditonals

---
+++ conditionalStatements/main.py
@@ -0,0 +1,31 @@
+# Conditional Expression = A one-line shortcut for the if-else statement (ternary operators)
+# Print or assign one of two values based on a condition
+# X if condition else Y
+
+num = 5
+print ("Positive" if num > 0 else "Negative")
+
+num = 2
+result = "EVEN" if num % 2 == 0 else "ODD"
+print(result)
+
+a = 6
+b = 7
+
+max_num = a if a > b else b
+print(max_num)
+
+min_num = a if a < b else b
+print(min_num)
+
+age = 21
+status = "Adult" if age >= 18 else "Child"
+print(status)
+
+temperature = 30
+weather = "HOT" if temperature > 20 else "COLD"
+print(weather)
+
+user_role = "admin"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+print(access_level)
